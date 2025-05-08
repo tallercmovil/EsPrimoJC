d5a551b884b6b15cd79e95673d4eef36aede76f5
Amaury PM <amaury.pema@gmail.com>
2025-05-08 09:48:42 -0600

    Ejercicio finalizado

--- app/src/main/java/com/amaurypm/esprimojc/MainActivity.kt
+++ app/src/main/java/com/amaurypm/esprimojc/MainActivity.kt
@@ -51,7 +51,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
         enableEdgeToEdge()
         setContent {
 
